598746f0db8bd3a37ea6bb02ab591f18df0bf671
Cassie C <cassie@colvindesign.com>
2021-01-21 18:44:38 -0500

    Initialized project using Spring Initializer

---
+++ src/main/java/com/cassie365/betterlearning/BetterlearningApplication.java
@@ -0,0 +1,13 @@
+package com.cassie365.betterlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterlearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cassie365/betterlearning/BetterlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassie365.betterlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
